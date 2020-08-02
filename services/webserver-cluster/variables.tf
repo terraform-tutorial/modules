e3924803e63240f8bc0ce0e451a36abf376f8b30
Eze Onuoha <onuohaeze@gmail.com>
2020-08-01 21:26:27 -0400

    initial commit of modules repo

---
+++ services/webserver-cluster/variables.tf
@@ -0,0 +1,45 @@
+variable "server_port" {
+  description = "The port the server will use for http requests"
+  type        = number
+  default     = 8080
+}
+variable "s3" {
+  description = "The alb access log bucket"
+  type        = string
+  default     = "chysome-terraform-state-file-bucket"
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+variable "db_remote_state_bucket" {
+  description = "The name of S3 bucket for the database's remote state"
+  type        = string
+}
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+  type        = string
+}
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g t2.micro)"
+  type        = string
+}
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+  type        = number
+}
+variable "max_size" {
+  description = "The maximum number of EC2 instances in the ASG"
+  type        = number
+}
+
+locals {
+  http_port = 80
+  ssh_port  = 22
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
