181b67fa788f4beda24773ec8d026fbbcf0501ee
Eze Onuoha <onuohaeze@gmail.com>
2020-08-07 18:41:29 -0400

    Adding if-else constructs

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -4,32 +4,68 @@ data "aws_vpc" "default"{
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*"
+    ]
+    resources = ["*"]
+  }
+}
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+  statement {
+    effect = "Allow"
+    actions = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+data "template_file" "user_data" {
+  count = var.enable_new_user_data ? 0 : 1
+  template = file("${path.module}/user-data.sh")
+
+  vars = {
+  server_port = var.server_port
+  db_address  = data.terraform_remote_state.db.outputs.address
+  db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
+data "template_file" "user_data_new" {
+  count = var.enable_new_user_data ? 1 : 0
+  template = file("${path.module}/user-data-new.sh")
+
+  vars = {
+  server_port = var.server_port
+  }
+}
 resource "aws_security_group" "instance" {
   name        = "${var.cluster_name}-instance"
 }
 resource "aws_security_group_rule" "app_access" {
-    type              = "ingress" 
-    security_group_id = aws_security_group.instance.id
-    from_port         = var.server_port
-    to_port           = var.server_port
-    protocol          = local.tcp_protocol
-    cidr_blocks       = local.all_ips
+  type              = "ingress" 
+  security_group_id = aws_security_group.instance.id
+  from_port         = var.server_port
+  to_port           = var.server_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
   }
 resource "aws_security_group_rule" "allow_ssh_inbound" {
-    type              = "ingress"
-    security_group_id = aws_security_group.instance.id
-    from_port         = local.ssh_port
-    to_port           = local.ssh_port
-    protocol          = local.tcp_protocol
-    cidr_blocks       = local.all_ips
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+  from_port         = local.ssh_port
+  to_port           = local.ssh_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
   }
 resource "aws_security_group_rule" "allow_ssh_outbound" {
-    type              = "egress" 
-    security_group_id = aws_security_group.instance.id
-    from_port         = local.any_port
-    to_port           = local.any_port
-    protocol          = local.tcp_protocol
-    cidr_blocks       = local.all_ips
+  type              = "egress" 
+  security_group_id = aws_security_group.instance.id
+  from_port         = local.any_port
+  to_port           = local.any_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
 }
 
 resource "aws_security_group" "alb" {
@@ -51,23 +87,15 @@ resource "aws_security_group_rule" "allow_http_outbound" {
     protocol    = local.tcp_protocol
     cidr_blocks = local.all_ips
 }
-
-
-data "template_file" "user_data" {
-    template = file("${path.module}/user-data.sh")
-
-    vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-    }
-}
-
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0ac80df6eff0e70b5"
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-  user_data       = data.template_file.user_data.rendered 
+  user_data       = (
+    length(data.template_file.user_data[*]) > 0
+    ? data.template_file.user_data[0].rendered
+    : data.template_file.user_data_new[0].rendered
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -147,6 +175,82 @@ resource "aws_lb_target_group" "asg" {
     unhealthy_threshold      = 2
 
   }
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+resource "aws_autoscaling_schedule" "scale_in-at-night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+  alarm_name = "${var.cluster_name}-high-cpu-utilization"
+  namespace = "AWS/EC2"
+  metric_name = "CPUUtilization"
+
+  dimensions = {
+    AutoScalingGroupName = "aws_autoscaling_group.example.name"
+  }
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 1
+  period = 300
+  statistic = "Average"
+  threshold = 90
+  unit = "Percent"
+}
+# If statement
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+  count = format("%.1s", var.instance_type) == "t" ? 1 : 0
+
+  alarm_name = "${var.cluster_name}-low-cpu-credit-baance"
+  namespace = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+
+  dimensions = {
+    AutoScalingGroupName = "aws_autoscaling_group.example.name"
+  }
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = 1
+  period = 300
+  statistic = "Minimum"
+  threshold = 10
+  unit = "Count"
+}
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name = "cloudwatch-read-only"
+  policy = data.aws_iam_policy_document.cloudwatch_read_only.json
+}
+resource "aws_iam_policy" "cloudwatch_full_access" {
+  name = "cloudwatch-read-only"
+  policy = data.aws_iam_policy_document.cloudwatch_full_access.json
+}
+
+
+# If-else statement 
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count = var.give_neo_cloudwatch_full_access ? 1 : 0
+
+  user = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn  
+}
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count = var.give_neo_cloudwatch_full_access ? 0 : 1
+
+  user = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn  
 }
 
 # data "terraform_remote_state" "db" {
--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -49,6 +49,18 @@ variable "custom_tags" {
   default     = {}
 }
 
+variable "enable_autoscaling" {
+  description = "if set to true, enable autoscaling"
+  type        = bool
+}
+variable "give_neo_cloudwatch_full_access"{
+  description = "If True, neo gets full access to cloudwatch"
+  type        = bool
+}
+variable "enable_new_user_data" {
+  description = "If set to true, use the new User Data Script"
+  type        = bool
+}
 
   
 
